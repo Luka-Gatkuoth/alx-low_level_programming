f0cb74ac0849868c6e3a617d498ddc1c17afd0c1
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-02-22 07:16:34 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -8,17 +7,7 @@ int main(void)
 {
 int i;
 i = 0;
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}
+positive_or_negative(i);
+
 return (0);
 }
