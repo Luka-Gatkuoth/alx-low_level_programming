e5a1cef10ffbd0ca3ebd0af002bb7485db34ef19
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-02-27 08:15:40 -0800

    a function that prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include <stdio.h>
+/**
+* print_rev - a function that prints a string, in reverse.
+* @str: The string to be reverse
+* Return: Nothing
+*/
+void print_rev(char *str)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+for (c -= 1; c >= 0; c--)
+{
+_putchar(s[c]);
+}
+_putchar('\n');
+}
